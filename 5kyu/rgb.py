0743b2aa139d1e184bb1d6ddd76f88d17a3af102
Enes Bayar <enesbayar04@gmail.com>
2025-09-01 19:13:46 +0300

    feat: New function

---
+++ 5kyu/rgb.py
@@ -0,0 +1,16 @@
+# The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+# Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+# Examples (input --> output):
+# 255, 255, 255 --> "FFFFFF"
+# 255, 255, 300 --> "FFFFFF"
+# 0, 0, 0       --> "000000"
+# 148, 0, 211   --> "9400D3"
+
+def rgb(r, g, b):
+    r = max(0, min(255, r))
+    g = max(0, min(255, g))
+    b = max(0, min(255, b))
+    
+    return "{:02X}{:02X}{:02X}".format(r, g, b)
