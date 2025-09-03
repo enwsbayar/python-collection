fed2b599fba69b4881df03888b8912c353d81b24
Enes Bayar <enesbayar04@gmail.com>
2025-09-03 23:59:52 +0300

    feat: Add digitize.py function to 8kyu folder

---
+++ 8kyu/digitize.py
@@ -0,0 +1,14 @@
+# Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+# Example (Input => Output):
+# 35231 => [1,3,2,5,3]
+# 0     => [0]
+
+def digitize(number):
+  result = []
+  for i in str(number):
+    result.append(int(i))
+  result.reverse()
+  return result
+
+print(digitize(35231))
