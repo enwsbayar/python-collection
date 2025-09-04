80ec12bd40c76f214aba97f98c9af67840bd4c1b
Enes Bayar <enesbayar04@gmail.com>
2025-09-04 14:51:31 +0300

    feat: Add rps.py function to 8kyu folder

---
+++ 8kyu/rps.py
@@ -0,0 +1,13 @@
+# Rock Paper Scissors
+# Let's play! You have to return which player won! In case of a draw return Draw!.
+
+# Examples(Input1, Input2 --> Output):
+
+# "scissors", "paper" --> "Player 1 won!"
+# "scissors", "rock" --> "Player 2 won!"
+# "paper", "paper" --> "Draw!"
+
+def rps(p1, p2):
+  return "Player 1 won!" if (p1 == "scissors" and p2 == "paper") or (p1 == "rock" and p2 == "scissors") or (p1 == "paper" and p2 == "rock") else "Player 2 won!" if p1 != p2 else "Draw!"
+
+print(rps("scissors", "paper"))
