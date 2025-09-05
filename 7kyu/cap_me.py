d00919223d53ee661a4811b87bfa7ca3e4c8c6e8
Enes Bayar <enesbayar04@gmail.com>
2025-09-06 00:36:16 +0300

    feat: Add cap_me.py solution to 5kyu folder

---
+++ 7kyu/cap_me.py
@@ -0,0 +1,9 @@
+# Create a function that accepts an array of names, and returns an array of each name with its first letter capitalized and the remainder in lowercase.
+
+# Examples
+# ["jo", "nelson", "jurie"] -->  ["Jo", "Nelson", "Jurie"]
+# ["KARLY", "DANIEL", "KELSEY"] --> ["Karly", "Daniel", "Kelsey"]
+
+def cap_me(arr):
+  return [w.lower().capitalize() for w in arr]
+print(cap_me(["jo", "nelson", "jurie"]))
