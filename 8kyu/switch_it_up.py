8ea54ecf47acdbbd48d53143fdb3825c5b51c63c
Enes Bayar <enesbayar04@gmail.com>
2025-09-05 22:04:17 +0300

    feat: Add switch_it_up.py solution to 8kyu folder

---
+++ 8kyu/switch_it_up.py
@@ -0,0 +1,8 @@
+# When provided with a number between 0-9, return it in words. Note that the input is guaranteed to be within the range of 0-9.
+
+# Input: 1
+
+# Output: "One".
+
+def switch_it_up(number):
+  return ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][number]
