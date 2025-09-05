2c717b8d7e4837fb557a512d1864d2a1fca21494
Enes Bayar <enesbayar04@gmail.com>
2025-09-06 00:00:11 +0300

    feat: Add reverse_words.py solution to 7kyu folder

---
+++ 7kyu/reverse_words.py
@@ -0,0 +1,10 @@
+# Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+# Examples
+# "This is an example!" ==> "sihT si na !elpmaxe"
+# "double  spaces"      ==> "elbuod  secaps"
+
+def reverse_words(text):
+  return "".join(w + " " for w in text[::-1].split()[::-1])[:-1] 
+
+print(reverse_words("This is an example!"))
