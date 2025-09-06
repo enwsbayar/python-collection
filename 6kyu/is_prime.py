d665fad25ff0895c0babe5390650e8e384e53725
Enes Bayar <enesbayar04@gmail.com>
2025-09-06 22:51:10 +0300

    feat: Add is_prime.py solution to 6kyu folder

---
+++ 6kyu/is_prime.py
@@ -0,0 +1,17 @@
+# Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime.
+
+# Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+# Requirements
+# You can assume you will be given an integer input.
+# You can not assume that the integer will be only positive. You may be given negative numbers as well ( or 0 ).
+# NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 ( or similar, depending on language ). Looping all the way up to n, or n/2, will be too slow.
+# Example
+# is_prime(1)  /* false */
+# is_prime(2)  /* true  */
+# is_prime(-1) /* false */
+
+def is_prime(number):
+  return False if number < 0 else sum([j for j in [i for i in range(1, number+1)] if number%j == 0]) == number + 1
+
+print(is_prime(13))
