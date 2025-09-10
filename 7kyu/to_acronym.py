a281e014f86369dfb5fc5db9ef1dff6c030baa39
Enes Bayar <enesbayar04@gmail.com>
2025-09-10 20:48:27 +0300

    feat: Add to_acronym.py solution to 7kyu folder

---
+++ 7kyu/to_acronym.py
@@ -0,0 +1,17 @@
+# Write function which takes a string and make an acronym of it.
+
+# Rule of making acronym in this kata:
+
+# split string to words by space char
+# take every first letter from word in given string
+# uppercase it
+# join them toghether
+# Eg:
+
+# Code wars -> C, w -> C W -> CW
+# Note: There will be at least two words in the given string!
+
+def to_acronym(inp):
+  return "".join([i[0].upper() for i in inp.split(" ")])
+
+print(to_acronym("hyper text markup language"))
