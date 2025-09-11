9fb4f165601dd64913e0db9c116bb01c8ef48193
Enes Bayar <enesbayar04@gmail.com>
2025-09-12 02:26:07 +0300

    feat: Add reverse.py solution to 8kyu folder

---
+++ 8kyu/reverse.py
@@ -0,0 +1,13 @@
+# You need to write a function that reverses the words in a given string. Words are always separated by a single space.
+
+# As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+# Example (Input --> Output)
+
+# "Hello World" --> "World Hello"
+# "Hi There." --> "There. Hi"
+
+def reverse(st):
+    return " ".join(st.split(" ")[::-1])
+
+print(reverse("Hello World"))
