dcf29bffed3b2886f3b11028b289fa1db7bc83b8
Enes Bayar <enesbayar04@gmail.com>
2025-09-21 00:30:44 +0300

    feat: Add exclamation_removal.py solution to 8kyu folder

---
+++ 8kyu/exclamation_removal.py
@@ -0,0 +1,15 @@
+# Description:
+# Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+# Examples
+# "Hi!"     ---> "Hi"
+# "Hi!!!"   ---> "Hi!!"
+# "!Hi"     ---> "!Hi"
+# "!Hi!"    ---> "!Hi"
+# "Hi! Hi!" ---> "Hi! Hi"
+# "Hi"      ---> "Hi"
+
+def remove(s):
+  return "" if not s else s[:-1] if s[-1] == "!" else s
+
+print(remove("!!Hi!!"))
