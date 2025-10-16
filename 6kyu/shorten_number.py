4e10d67c547f8a6c638e252a70903f68310945d3
Enes Bayar <enesbayar04@gmail.com>
2025-10-16 06:01:03 +0300

    feat: Add shorten_number.py solution to 6kyu folder

---
+++ 6kyu/shorten_number.py
@@ -0,0 +1,31 @@
+# Here is a new kata that Codewars asked me to do related to interviewing and working in a production setting.
+
+# You might be familar with and have used Angular.js. Among other things, it lets you create your own filters that work as functions. You can then put these in a page to perform specific data changes, such as shortening a number to display a more concise notation.
+
+# In this kata, we will create a function which returns another function (or process, in Ruby) that shortens very long numbers. Given an initial array of values replace the Xth power of a given base. If the input of the returned function is not a numerical string, it should return the input itself as a string.
+
+# Here's an example, which is worth a thousand words:
+
+# filter1 = shorten_number(['','k','m'],1000)
+# filter1('234324') == '234k'
+# filter1('98234324') == '98m'
+# filter1([1,2,3]) == '[1,2,3]'
+# filter2 = shorten_number(['B','KB','MB','GB'],1024)
+# filter2('32') == '32B'
+# filter2('2100') == '2KB';
+# filter2('pippi') == 'pippi'
+
+def shorten_number(suffixes, base):
+  def inner(value):
+    if not isinstance(value, str) or not value.isdigit():
+      return str(value)
+
+    num = int(value)
+    i = 0
+
+    while num >= base and i < len(suffixes) - 1:
+      num //= base
+      i += 1
+
+    return f"{num}{suffixes[i]}"
+  return inner
