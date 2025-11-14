b90c1dad2faa379f64fbcf91d2a83bc30942af53
Enes Bayar <enesbayar04@gmail.com>
2025-11-14 20:03:20 +0300

    feat: Add is_even function to determine if a number is even

---
+++ 8kyu/is_even.py
@@ -0,0 +1,10 @@
+# Problem:
+# Verilen bir tamsayı için sayı çift ise True, tek ise False döndüren bir fonksiyon yazın.
+
+def is_even(n):
+  return n % 2 == 0
+
+# Örnekler
+# is_even(2) -> True
+# is_even(3) -> False
+# is_even(0) -> True
