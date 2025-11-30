58ef5f1b222c16d375ae72582f3edcf889827940
Enes Bayar <enesbayar04@gmail.com>
2025-11-30 22:00:46 +0300

    feat: Implement switcheroo function to swap letters a and b in a string

---
+++ 7kyu/switcheroo.py
@@ -0,0 +1,17 @@
+# Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+# Example:
+
+# 'acb' --> 'bca'
+# 'aabacbaa' --> 'bbabcabb'
+
+def switcheroo(s):
+  result = []
+  for char in s:
+    if char == 'a':
+      result.append('b')
+    elif char == 'b':
+      result.append('a')
+    else:
+      result.append(char)
+  return ''.join(result)
