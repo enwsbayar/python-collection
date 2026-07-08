d572f9d8d2321148974cc014addea37880cea470
Enes Bayar <enesbayar04@gmail.com>
2026-07-08 21:18:37 +0300

    feat: implement max_sequence function to find maximum sum of contiguous subarray

---
+++ 5kyu/max_sequence2.py
@@ -0,0 +1,20 @@
+# The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+# For example:
+
+# Input: [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+# Output: 6 (Sum of [4, -1, 2, 1])
+# Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead. Your solution should be fast, it will be tested on very large arrays so slow solutions will time out.
+
+# Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+
+
+def max_sequence(arr):
+	best = 0
+	current = 0
+
+	for value in arr:
+		current = max(0, current + value)
+		best = max(best, current)
+
+	return best
